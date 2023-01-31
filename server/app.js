6a4c4121e2e6276c51bbf18109076d09046462b7
JeffeVargasP <jeffevargas@proton.me>
2023-01-31 07:26:35 -0300

    CRIADO: Rota de status criada

--- server/app.js
+++ server/app.js
@@ -1,4 +1,45 @@
+require("dotenv-safe").config();
+
 const express = require("express");
-const app = express.Router();
+const helmet = require("helmet");
+const morgan = require("morgan");
+const statusRoute = require("./routes/statusRoute");
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(helmet());
+app.use(express.json());
+
+app.use((req, res, nxt) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  nxt();
+});
+
+app.use("/status", statusRoute);
+
+app.use((req, res, nxt) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  nxt(error);
+});
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -4,13 +4,7 @@ const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 const port = process.env.PORT || 3001;
-const production = process.env.PRODUCTION;
-const url = process.env.SERVER_URL;
 
-server.listen(port, function callback() {
-  if (production) {
-    console.log(`Server is running at ${url}`);
-  } else {
-    console.log(`Server is running at http://localhost:${port}`);
-  }
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
